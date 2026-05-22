619468b93aaadaa331e49b656d0a39c0d8a0b5f4
t01089402044-commits <t01089402044@gmail.com>
2026-05-22 15:03:06 +0900

    fix public path

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(compression());
 app.use(express.json({ limit: '30mb' }));
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/api/health', (req, res) => res.json({ ok: true }));
 
 let browser = null;
